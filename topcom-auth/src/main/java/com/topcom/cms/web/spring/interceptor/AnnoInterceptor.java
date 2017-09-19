ec48d7acf9d9f54cbd67b9a9f4ba42386bb21b08
tabtou <807507917@qq.com>
2017-09-19 11:47:56 +0800

    //1.4以后取消了location 两种解决方案 1如下替代    // 2监听ApplicationEnvironmentPreparedEvent事件，通过ResourceLoader加载自定义的配置文件

--- topcom-auth/src/main/java/com/topcom/cms/web/spring/interceptor/AnnoInterceptor.java
+++ topcom-auth/src/main/java/com/topcom/cms/web/spring/interceptor/AnnoInterceptor.java
@@ -2,12 +2,19 @@ package com.topcom.cms.web.spring.interceptor;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by lism on 17-6-13.
  */
-@ConfigurationProperties(prefix = "anno",locations = "classpath:auth.properties")
+//@ConfigurationProperties(prefix = "anno",locations = "classpath:auth.properties")
+    //1.4以后取消了location 两种解决方案 1如下替代
+   // 2监听ApplicationEnvironmentPreparedEvent事件，通过ResourceLoader加载自定义的配置文件
 @Configuration
+@Component
+@PropertySource("classpath:auth.properties")
+@ConfigurationProperties(prefix="anno")
 public class AnnoInterceptor {
 
     private String[] excludePaths;
