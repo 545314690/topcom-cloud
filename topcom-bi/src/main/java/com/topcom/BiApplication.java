98492ecb90d9c59b81a0ea6c839c05329965d294
llsm <545314690@qq.com>
2018-02-28 15:40:22 +0800

    topcom-bi 接口管理

---
+++ topcom-bi/src/main/java/com/topcom/BiApplication.java
@@ -0,0 +1,18 @@
+package com.topcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableCircuitBreaker
+public class BiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiApplication.class, args);
+	}
+}
